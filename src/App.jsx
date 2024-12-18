b22a3d92b7a7f0093ae6cb119d99940ed470c041
lokeshloki3 <lokesh1992p@gmail.com>
2024-12-18 15:57:06 +0530

    Routes for home and Cards page created

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/girmanreact" element={<Home />} />
-        {/* <Route path="/cards" element={<Cards />} /> */}
+        <Route path="/cards" element={<Cards />} />
       </Routes>
     </div>
   );
